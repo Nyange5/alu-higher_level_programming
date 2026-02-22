8859125b094cd670527a8f21af2963a0c5d83447
Nyange5 <p.ishimwe3@alustudent.com>
2026-02-22 12:15:34 -0800

    Changes

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import marshal
-    import os
-
-    pyc_path = os.path.join(os.path.dirname(__file__), "__pycache__", "hidden_4.cpython-38.pyc")
-
-    with open(pyc_path, "rb") as f:
-        f.read(16)
-        code = marshal.load(f)
-
-    names = [n for n in code.co_names if not n.startswith("__")]
-
-    for n in sorted(names):
-        print(n)
+    import hidden_4
+    names = dir(hidden_4)
+    names.sort()  # sorting alphabetically
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
