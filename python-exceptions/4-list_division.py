fd8bff08c8038d6eae1319e23942d2c12b966829
Nyange5 <p.ishimwe3@alustudent.com>
2026-03-17 11:55:14 -0700

    Changes

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         result = 0
         try:
-            result = my_list_1[i] / my_list_2[i]
+            x = my_list_1[i]
+            y = my_list_2[i]
+            if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+                raise TypeError
+            result = x / y
         except TypeError:
             print("wrong type")
         except ZeroDivisionError:
