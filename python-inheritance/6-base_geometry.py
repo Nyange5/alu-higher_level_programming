c4a00ebe9f29793852f8c3adeea4bdb2826d1d58
Nyange5 <p.ishimwe3@alustudent.com>
2026-03-29 05:24:38 -0700

    New Files

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method"""
+
+    def area(self):
+        """Raises Exception - area() is not implemented"""
+        raise Exception("area() is not implemented")
