edfa1111a73e8218ddb24355b1e52faf11886e62
Nyange5 <p.ishimwe3@alustudent.com>
2026-03-29 09:30:43 -0700

    New Files

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Load from JSON file module"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates a Python object from a JSON file"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
