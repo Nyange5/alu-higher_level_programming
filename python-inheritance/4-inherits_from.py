48b97e5fedb436b7598f7269f5a6dfb8104dce5a
Nyange5 <p.ishimwe3@alustudent.com>
2026-03-29 05:42:56 -0700

    Changes

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""inherits_from module"""
+"""Module that checks if an object inherits from a class."""
 
 
 def inherits_from(obj, a_class):
-    """Returns True if obj is instance of a class that inherited from a_class"""
+    """Return True if obj is an instance of a subclass of a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
