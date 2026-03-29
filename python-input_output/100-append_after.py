8ea8807cc2b8e658357adc01a96e2cb25e1122f9
Nyange5 <p.ishimwe3@alustudent.com>
2026-03-29 09:41:33 -0700

    New Files

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Append after module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text after each line containing a specific string"""
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open(filename, "w", encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
